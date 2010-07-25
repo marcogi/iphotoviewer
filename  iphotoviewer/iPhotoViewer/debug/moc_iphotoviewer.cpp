b82f07eb6b53162ee22ac377911e85c490787f41
davrummel <davrummel@bd7ddce4-d143-477b-d267-beacb3aef6bf>
2010-07-25 12:06:06 +0000

    +resizing now works for the thumbnail view

---  iphotoviewer/iPhotoViewer/debug/moc_iphotoviewer.cpp
+++  iphotoviewer/iPhotoViewer/debug/moc_iphotoviewer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'iphotoviewer.h'
 **
-** Created: Sun Jul 25 12:09:46 2010
+** Created: Sun Jul 25 14:02:57 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
---  iphotoviewer/iPhotoViewer/debug/moc_photopanel.cpp
+++  iphotoviewer/iPhotoViewer/debug/moc_photopanel.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'photopanel.h'
 **
-** Created: Sat Jul 24 14:35:12 2010
+** Created: Sun Jul 25 14:02:54 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
