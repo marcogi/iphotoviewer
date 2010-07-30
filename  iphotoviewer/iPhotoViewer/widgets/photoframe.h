833473d0fd08bde03c609da98ba2a6bd80d66ee8
davrummel <davrummel@bd7ddce4-d143-477b-d267-beacb3aef6bf>
2010-07-30 18:19:41 +0000

    +some further improvements in thumbnailsview +fixed a minor bug when changing the roll or album while in photo view

---  iphotoviewer/iPhotoViewer/widgets/photoframe.h
+++  iphotoviewer/iPhotoViewer/widgets/photoframe.h
@@ -1,6 +1,9 @@
 #ifndef PHOTOFRAME_H
 #define PHOTOFRAME_H
 
+#define CSS_ROUND "QLabel#photoWidget { border-width: 3px; border-style: solid; border-color: #000000; border-radius: 10px; color: #000000;}"
+#define CSS_EDGE "QLabel#photoWidget { border-width: 1px; border-style: solid; border-color: #000000; border-radius: 0px; color: #000000;}"
+
 #include <QtGui/QWidget>
 #include <QTextStream>
 #include <QPixmap>
